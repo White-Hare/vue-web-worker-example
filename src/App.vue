84fc0b162cc3db5374d03061ff5b367a4426ffe2
Mete Arslan <mete.arslan@devops.ncia.nato.int>
2023-09-19 17:56:06 +0300

    date added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { provide, ref, watch } from 'vue';
 import ListItem from './components/ListItem.vue'
+import Title from './components/Title.vue'
 import { Item } from './models/item.model';
 import { ITEMS_KEY } from './inject-keys';
 import { Association } from './models/association.model';
@@ -12,6 +13,8 @@ import { arrayToDict } from './helpers/array-to-dict';
 
 const numberOfItems = ref(500);
 const assosicationPerItem = ref(5);
+const enableWebWorkers = ref(true);
+const generationTime = ref(0);
 
 
 const items = ref([] as Item[]);
@@ -19,9 +22,13 @@ const assosications = ref([] as Association[]);
 const itemsDict = ref({} as Record<string, Item>);
 
 function updateItems(numberOfItems: number, assosicationPerItem: number) {
+  const startTime = Date.now();
+  
   items.value = addRandomAssosicationsToItems(createExapmleItems(numberOfItems), assosicationPerItem);
   itemsDict.value = arrayToDict(items.value);
   assosications.value = getAssosications(items.value, itemsDict.value);
+  
+  generationTime.value = (Date.now() - startTime) / 1000;
 }
 
 provide(ITEMS_KEY, itemsDict);
@@ -32,17 +39,21 @@ watch([numberOfItems, assosicationPerItem], () => updateItems(numberOfItems.valu
 
 
 <template>
-  <div class="grid grid-cols-2 text-white bg-gray-900">
-    <Panel title="Items" input-title="Number Of Items" v-model="numberOfItems">
-      <div class="grid grid-cols-3 p-5 gap-3">
-        <ListItem v-for="item in items" :title="item.title" :content="item.content" :number-of-views="item.numberOfViews"
-          :related-item-ids="item.relatedItemIds" />
-      </div>
-    </Panel>
-    <Panel title="Associations" input-title="Associations Per Item" v-model="assosicationPerItem">
-      <div class="grid grid-cols-3 p-5 gap-3">
-        <ListItem v-for="item in assosications" :title="item.title" />
-      </div>
-    </Panel>
+
+  <div>
+    <Title v-model="enableWebWorkers" :generation-time="generationTime"/>
+    <div class="grid grid-cols-2 text-white bg-gray-900">
+      <Panel title="Items" input-title="Number Of Items" v-model="numberOfItems">
+        <div class="grid grid-cols-3 p-5 gap-3">
+          <ListItem v-for="item in items" :title="item.title" :content="item.content"
+            :number-of-views="item.numberOfViews" :related-item-ids="item.relatedItemIds" />
+        </div>
+      </Panel>
+      <Panel title="Associations" input-title="Associations Per Item" v-model="assosicationPerItem">
+        <div class="grid grid-cols-3 p-5 gap-3">
+          <ListItem v-for="item in assosications" :title="item.title" />
+        </div>
+      </Panel>
+    </div>
   </div>
 </template>
---
+++ src/components/Title.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+defineProps({
+    generationTime: {
+        type: Number,
+        default: 0
+    },
+    enableWebWorkers: Boolean
+});
+defineEmits(['update:enableWebWorkers']);
+
+</script>
+
+<template>
+    <div class="flex flex-row justify-between mx-2 py-3">
+        <div>
+            <div class="flex flex-row">
+                <label for="enableWebWorkers">
+                    <h2 class="text-lg font-bold">
+                        Enable Web Workers:
+                    </h2>
+                </label>
+                <input :value="enableWebWorkers" @input="$emit('update:enableWebWorkers', ($event.target as any)?.value)"
+                    name="enableWebWorkers" class="ml-3" type="checkbox" />
+            </div>
+        </div>
+        <div>
+            <h1 class="text-2xl font-bold">
+                Web Workers
+            </h1>
+        </div>
+        <div>
+            <h2 class="text-lg font-bold">
+                Generation Time: {{ generationTime }}s
+            </h2>
+        </div>
+    </div>
+</template>

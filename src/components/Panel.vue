06cae861ec5a2ff15573f7ed03e1f0ff0d472a71
Mete Arslan <mete.arslan@devops.ncia.nato.int>
2023-09-20 09:47:19 +0300

    render items

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+
 defineProps({ modelValue: Number, title: String, inputTitle: String });
 defineEmits(['update:modelValue']);
 
+const renderItems = ref(true)
 
 </script>
 
@@ -10,13 +13,21 @@ defineEmits(['update:modelValue']);
         <h2 class="text-xl font-bold underline p-3 text-center">
             {{ title }}
         </h2>
-        <div class="px-5 flex flex-col">
-            <label for="modelValue">{{ inputTitle }}</label>
-            <input :value="modelValue" @input="$emit('update:modelValue', ($event.target as any)?.value)"
-                name="modelValue" class="text-black w-1/3" type="number" />
+        <div class="px-5 flex flex-row">
+            <div class="flex flex-col">
+                <label for="modelValue">{{ inputTitle }}</label>
+                <input :value="modelValue" @input="$emit('update:modelValue', ($event.target as any)?.value)"
+                    name="modelValue" class="text-black w-2/3" type="number" />
+            </div>
+            <div class="flex flex-col">
+                <label for="renderItems">Render Items</label>
+                <input v-model="renderItems" class="w-fit" name="renderItems" type="checkbox" />
+            </div>
         </div>
         <div>
-            <slot />
+            <div v-if="renderItems">
+                <slot/>
+            </div>
         </div>
     </div>
 </template>
